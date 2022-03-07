85197d701a0a634791e6c3e3e51eaf52c0e6afb1
RJudge1170 <ryantj@live.unc.edu>
2022-03-06 23:16:48 -0500

    added flip endpoint

--- server.js
+++ server.js
@@ -7,13 +7,28 @@ const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%', port))
 })
 
+function coinFlip() {
+  
+    let x = Math.floor(Math.random() * 10);
+  
+    if (x < 5) {
+      return 'heads';
+    } else {
+      return 'tails';
+    }
+  }
+
 app.get('/app', (req, res) => {
     res.status(200).end('OK')
     res.type('text/plain')
 })
 
 app.get('/app/echo/:number', (req, res) => {
-    res.status(200).json({ 'message': req.params.number })
+    res.status(200).json({ 'message' : req.params.number })
+})
+
+app.get('/app/flip', (req, res) => {
+    res.status(200).json({ 'flip' : coinFlip() })
 })
 
 app.use(function(req, res) {
